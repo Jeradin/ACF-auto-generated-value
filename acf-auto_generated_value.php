884307f4de9d1085fa1994d1edb01f611dbc1719
Geet Jacobs <geetjacobs@gmail.com>
2015-05-19 15:44:11 -0600

    Removed support for anything below 5.0

--- acf-auto_generated_value.php
+++ acf-auto_generated_value.php
@@ -1,22 +1,15 @@
 <?php
 
 /*
-Plugin Name: Advanced Custom Fields: Auto-generated value
-Plugin URI: https://github.com/andersthorborg/
-Description: Creates a hidden/readonly field in a repeater that gets a unique key, that won't change once it is saved. Ideal for user-generated forms. Includes option to hide the field from the user.
+Plugin Name: Advanced Custom Fields: Generate Random Key value
+Plugin URI: https://github.com/Jeradin/ACF-auto-generated-value
+Description: Creates a hidden/readonly field that generates a unique key, that won't change once it is saved. Ideal for user-generated forms. Includes option to hide the field from the user.
 Version: 1.0.0
-Author: Kindly
+Author: Kindly, Jeradin
 Author URI: http://kindly.dk
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
-
-
-
-
-// 1. set text domain
-// Reference: https://codex.wordpress.org/Function_Reference/load_plugin_textdomain
-load_plugin_textdomain( 'acf-auto_generated_value', false, dirname( plugin_basename(__FILE__) ) . '/lang/' );
 
 
 
@@ -30,18 +23,6 @@ function include_field_types_auto_generated_value( $version ) {
 }
 
 add_action('acf/include_field_types', 'include_field_types_auto_generated_value');
-
-
-
-
-// 3. Include field type for ACF4
-function register_fields_auto_generated_value() {
-
-	include_once('acf-auto_generated_value-v4.php');
-
-}
-
-add_action('acf/register_fields', 'register_fields_auto_generated_value');
 
 
 
